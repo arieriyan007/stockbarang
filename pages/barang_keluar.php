1416c6e2a2f45ccc2838c3e237a6670948511147
arieriyan007 <arieriyan2010@gmail.com>
2023-09-07 14:45:18 +0800

    menambahkan script di line 16, 36 sampai dengan 45. untuk memberi alert jika barang keluar tidak bisa melebihi dari stock yang tersedia

--- pages/barang_keluar.php
+++ pages/barang_keluar.php
@@ -12,21 +12,36 @@ if (isset($_POST['addKeluar'])) {
 
     // periksa stok barang sekarang
     $stocksekarang = $keepstock['stock'];
-    // pengurangan stock
-    $stockkurang = $stocksekarang-$qty;
 
-    // data inputan disimpan kedatabase data yg dikurangin
-    $datastocksekarang = mysqli_query($koneksi, "INSERT INTO keluar (idbarang, qty, penerima) VALUES
-    ('$barang','$qty','$penerima')");
+    // membuat if agar barang keluar tidak boleh lebih besar dari stock sekarang, agar nantinya tidak minus
+    if ($stocksekarang >= $qty) {
+        // jika barang cukup maka jlnkan proses di bawah ini
+        // di lanjut dengan pengurangan stock
+        $stockkurang = $stocksekarang-$qty;
+
+        // data inputan disimpan kedatabase data yg dikurangin
+        $datastocksekarang = mysqli_query($koneksi, "INSERT INTO keluar (idbarang, qty, penerima) VALUES
+        ('$barang','$qty','$penerima')");
+        
+        // logika saat barang di update setelah dikurangkan
+        $updatestock = mysqli_query($koneksi, "UPDATE stock SET stock='$stockkurang' WHERE idbarang='$barang' ");
+
+        // kembalikan halaman
+        if ($datastocksekarang&&$updatestock) {
+            header("location:keluar.php?pesan=berhasil");
+        } else {
+            header("location:keluar.php?pesan=gagalupdate");
+        }
     
-    // logika saat barang di update setelah dikurangkan
-    $updatestock = mysqli_query($koneksi, "UPDATE stock SET stock='$stockkurang' WHERE idbarang='$barang' ");
-
-    // kembalikan halaman
-    if ($datastocksekarang&&$updatestock) {
-        header("location:keluar.php?pesan=berhasil");
     } else {
-        header("location:keluar.php?pesan=gagalupdate");
+        // jika barang tidak cukup maka (else), disini kita menggabungkan bahasa menggunakna javascript sebagai alertnya
+        echo "
+        <script>
+            alert('Stock barang saat ini tidak mencukupi ! ');
+            window.location.href='keluar.php';
+        </script>
+        ";
+
     }
 }
 ?>
